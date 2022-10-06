a24c55af6161bc443281ba98384926053efa40a2
zweidmitr <zweidmitr@gmail.com>
2022-10-06 15:01:55 +0300

    feat: implementing a sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -35,3 +35,28 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle('nav-open')
   })
 })
+
+////////////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector('.section-hero')
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0]
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky')
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove('sticky')
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+)
+obs.observe(sectionHeroEl)
